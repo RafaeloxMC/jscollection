2779ac340b4a8d28b59d06edecce128c570f3e3d
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-11-01 18:06:29 +0100

    Fill empty params

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -9,14 +9,14 @@ function CardContainer() {
 				name="Anne.JPG"
 				description="The famous Anne.JPG screamer!"
 			/>
-			<JSCard />
-			<JSCard />
-			<JSCard />
-			<JSCard />
-			<JSCard />
-			<JSCard />
-			<JSCard />
-			<JSCard />
+			<JSCard id={""} name={"2"} description={"2"} />
+			<JSCard id={""} name={"3"} description={"3"} />
+			<JSCard id={""} name={"4"} description={"4"} />
+			<JSCard id={""} name={"5"} description={"5"} />
+			<JSCard id={""} name={"6"} description={"6"} />
+			<JSCard id={""} name={"7"} description={"7"} />
+			<JSCard id={""} name={"8"} description={"8"} />
+			<JSCard id={""} name={"9"} description={"9"} />
 		</div>
 	);
 }
