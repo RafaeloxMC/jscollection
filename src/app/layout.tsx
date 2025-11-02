c295ecf15cf033219c0a1d841ae3788e85bcb20b
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-11-02 20:19:47 +0100

    Did I really spell my own website's name wrong?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-	title: "JSCollective",
+	title: "JSCollection",
 	description: "Your favourite website on the internet!",
 };
 
