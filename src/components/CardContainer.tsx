d27a7be23d0785e8262ec57ec6af4e4df133f0e0
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-11-02 09:48:36 +0100

    Use array with screamers instead of  HTML elements

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,26 +1,20 @@
 import React from "react";
 import JSCard from "./JSCard";
+import { js } from "@/util/js";
 
 function CardContainer() {
 	return (
 		<div className="grid grid-cols-3 w-full gap-4 px-8 py-4">
-			<JSCard
-				id="anne"
-				name="Anne.JPG"
-				description="The famous Anne.JPG screamer!"
-			/>
-			<JSCard
-				id={"car"}
-				name={"Relaxing car drive"}
-				description={"What could possibly go wrong here?"}
-			/>
-			<JSCard id={""} name={"3"} description={"3"} />
-			<JSCard id={""} name={"4"} description={"4"} />
-			<JSCard id={""} name={"5"} description={"5"} />
-			<JSCard id={""} name={"6"} description={"6"} />
-			<JSCard id={""} name={"7"} description={"7"} />
-			<JSCard id={""} name={"8"} description={"8"} />
-			<JSCard id={""} name={"9"} description={"9"} />
+			{js.map((screamer, index) => {
+				return (
+					<JSCard
+						id={screamer.id}
+						description={screamer.description}
+						name={screamer.description}
+						key={screamer.id + "-" + index}
+					/>
+				);
+			})}
 		</div>
 	);
 }
---
+++ src/util/js.ts
@@ -0,0 +1,14 @@
+import { IScreamer } from "./types";
+
+export const js: IScreamer[] = [
+	{
+		id: "anne",
+		name: "Anne.JPG",
+		description: "The famous Anne.JPG screamer!",
+	},
+	{
+		id: "car",
+		name: "Relaxing Car Drive",
+		description: "What could possibly go wrong?!",
+	},
+];
